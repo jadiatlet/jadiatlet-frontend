949e74a69998f699d60fd02ea6de03e36ccf8296
masagus hariadi arief <adi.arief77@gmail.com>
2019-01-29 18:28:55 +0700

    initial home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,14 +1,62 @@
-import React from 'react'
-import Navbar from '../Navbar'
+import React, { Fragment } from "react";
+import Navbar from "../Navbar";
 
-import { Container } from 'semantic-ui-react'
+import { Header } from "semantic-ui-react";
+import { Button, Form, Container } from "semantic-ui-react";
+
+import "./index.css";
+
+const countryOptions = [
+  { text: "Jakarta Selatan" },
+  { text: "Jakarta Barat" },
+  { text: "Jakarta Timur" },
+  { text: "Depok" }
+];
+
+const sportOptions = [
+  { text: "Lempar Kuda" },
+  { text: "Lompat Pendek" },
+  { text: "Angkat Kunam" }
+];
 
 const Home = () => {
   return (
-    <Container>
-      <Navbar />
-    </Container>
-  )
-}
+    <Fragment>
+      <Container>
+        <Navbar />
+      </Container>
 
-export default Home
+      <div className="homeBackground">
+        <section className="homeSection">
+          <div className="left-side">
+            <img src="/assets/home/banner_img.png" alt="" />
+          </div>
+
+          <div className="right-side">
+            <Header className="section-title" as="h1">
+              Ready for Jadi Atlet ?
+            </Header>
+
+            <Form success>
+              <Form.Select
+                label="Select your location"
+                placeholder="Select your country"
+                options={countryOptions}
+              />
+
+              <Form.Select
+                label="Select your sport"
+                placeholder="Select your sport"
+                options={sportOptions}
+              />
+
+              <Button>Search</Button>
+            </Form>
+          </div>
+        </section>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Home;
