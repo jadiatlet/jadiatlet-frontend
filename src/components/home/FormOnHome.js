44b9da5a1bb7bdea40c1a3704e23cda34ff72cc4
masagus hariadi arief <adi.arief77@gmail.com>
2019-01-30 08:36:13 +0700

    make responsive css for home

---
+++ src/components/home/FormOnHome.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Grid, Button, Form, Header } from "semantic-ui-react";
+
+import "./index.css";
+
+const countryOptions = [
+  { text: "Jakarta Selatan" },
+  { text: "Jakarta Barat" },
+  { text: "Jakarta Timur" },
+  { text: "Depok" }
+];
+
+const sportOptions = [
+  { text: "Lempar Kuda" },
+  { text: "Lompat Pendek" },
+  { text: "Angkat Kunam" }
+];
+
+const FormOnHome = () => (
+  <div className="container-FormOnHome">
+    <Header className="section-title" as="h1">
+      Ready for Jadi Atlet ?
+    </Header>
+
+    <Form success>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={10}>
+            <Form.Select
+              label="Select your location"
+              placeholder="Select your country"
+              options={countryOptions}
+            />
+
+            <Form.Select
+              label="Select your sport"
+              placeholder="Select your sport"
+              options={sportOptions}
+            />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <Button>Search</Button>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Form>
+  </div>
+);
+
+export default FormOnHome;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,23 +1,10 @@
 import React, { Fragment } from "react";
-import Navbar from "../Navbar";
+import { Image, Grid, Container } from "semantic-ui-react";
 
-import { Header } from "semantic-ui-react";
-import { Button, Form, Container } from "semantic-ui-react";
+import Navbar from "../Navbar";
+import FormOnHome from "./FormOnHome";
 
 import "./index.css";
-
-const countryOptions = [
-  { text: "Jakarta Selatan" },
-  { text: "Jakarta Barat" },
-  { text: "Jakarta Timur" },
-  { text: "Depok" }
-];
-
-const sportOptions = [
-  { text: "Lempar Kuda" },
-  { text: "Lompat Pendek" },
-  { text: "Angkat Kunam" }
-];
 
 const Home = () => {
   return (
@@ -25,35 +12,16 @@ const Home = () => {
       <Container>
         <Navbar />
       </Container>
-
       <div className="homeBackground">
-        <section className="homeSection">
-          <div className="left-side">
-            <img src="/assets/home/banner_img.png" alt="" />
-          </div>
+        <Grid centered columns={2}>
+          <Grid.Column floated="left">
+            <Image src="/assets/home/banner_img.png" alt="" />
+          </Grid.Column>
 
-          <div className="right-side">
-            <Header className="section-title" as="h1">
-              Ready for Jadi Atlet ?
-            </Header>
-
-            <Form success>
-              <Form.Select
-                label="Select your location"
-                placeholder="Select your country"
-                options={countryOptions}
-              />
-
-              <Form.Select
-                label="Select your sport"
-                placeholder="Select your sport"
-                options={sportOptions}
-              />
-
-              <Button>Search</Button>
-            </Form>
-          </div>
-        </section>
+          <Grid.Column floated="right">
+            <FormOnHome />
+          </Grid.Column>
+        </Grid>
       </div>
     </Fragment>
   );
