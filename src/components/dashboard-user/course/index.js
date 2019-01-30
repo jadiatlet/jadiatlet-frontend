43fe443e5ab9d14ddb0fe93d6e38bcaa7411422e
masagus hariadi arief <adi.arief77@gmail.com>
2019-01-30 16:05:31 +0700

    add directory

---
+++ src/components/dashboard-user/course/index.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import { Container } from "semantic-ui-react";
+
+import NavbarLogedin from "../../NavbarLogedin";
+
+const dashboarUser = () => {
+  return (
+    <Fragment>
+      <Container>
+        <NavbarLogedin />
+      </Container>
+
+      <div>
+        <span>knsklnsdk</span>
+      </div>
+    </Fragment>
+  );
+};
+
+export default dashboarUser;
