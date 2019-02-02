68b07c69b64460bc665e50b7a50227b34ac266ce
masagus hariadi arief <adi.arief77@gmail.com>
2019-02-02 13:50:35 +0700

    user dashboard first init

--- src/components/dashboard-user/course/index.js
+++ src/components/dashboard-user/course/index.js
@@ -2,17 +2,21 @@ import React, { Fragment } from "react";
 import { Container } from "semantic-ui-react";
 
 import NavbarLogedin from "../../NavbarLogedin";
+import MenuBarUSer from "../../MenuBarUser";
+import "../index.css";
 
 const dashboarUser = () => {
   return (
     <Fragment>
       <Container>
         <NavbarLogedin />
+        <div className="userCourseBackgroud">
+          <MenuBarUSer />
+          <div className="mainSection">
+            <span>its just temporary</span>
+          </div>
+        </div>
       </Container>
-
-      <div>
-        <span>knsklnsdk</span>
-      </div>
     </Fragment>
   );
 };
