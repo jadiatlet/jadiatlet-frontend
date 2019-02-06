2848e36228a404c5a46c26b11d8f5caefad82588
Panji Gumelar <panjiggm@gmail.com>
2019-02-06 20:04:11 +0700

    complete first ver modal coach experience & achievement

--- src/components/DashboardCoach/Profile/CoachAchievements.js
+++ src/components/DashboardCoach/Profile/CoachAchievements.js
@@ -1,38 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Divider, Button, Icon, Container, Table, Header, Segment } from 'semantic-ui-react'
 
-const CoachAchievements = props => {
-  return (
-    <Segment>
-      <Container textAlign="right">
-        <Button color="twitter">
-          <Icon name="add circle" /> Add Achievement
-        </Button>
-      </Container>
+import ModalAchievement from './Modal/ModalAchievement'
 
-      <Divider horizontal>
-        <Header as="h5">Coach Achievemnets</Header>
-      </Divider>
+class CoachAchievements extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { open: false }
+  }
 
-      <Table basic>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Number</Table.HeaderCell>
-            <Table.HeaderCell>Achievement Tittle</Table.HeaderCell>
-            <Table.HeaderCell>Year</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+  show = () => this.setState({ open: true })
+  close = () => this.setState({ open: false })
 
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>1</Table.Cell>
-            <Table.Cell>Jajang</Table.Cell>
-            <Table.Cell>2017</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
-    </Segment>
-  )
+  render() {
+    const { open } = this.state
+
+    return (
+      <Segment>
+        <ModalAchievement status={open} handleClose={this.close} />
+        <Container textAlign="right">
+          <Button color="twitter" onClick={this.show}>
+            <Icon name="add circle" /> Add Achievement
+          </Button>
+        </Container>
+
+        <Divider horizontal>
+          <Header as="h5">Coach Achievemnets</Header>
+        </Divider>
+
+        <Table basic>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Number</Table.HeaderCell>
+              <Table.HeaderCell>Achievement Tittle</Table.HeaderCell>
+              <Table.HeaderCell>Year</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>1</Table.Cell>
+              <Table.Cell>Jajang</Table.Cell>
+              <Table.Cell>2017</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </Segment>
+    )
+  }
 }
 
 export default CoachAchievements
