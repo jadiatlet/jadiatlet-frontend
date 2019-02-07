4e570e69ba14f95ecc332c8ce392dfaef687362e
masagus hariadi arief <adi.arief77@gmail.com>
2019-02-07 15:36:15 +0700

    change seach feature behavior in coach

--- src/components/DashboardCoach/Course/index.js
+++ src/components/DashboardCoach/Course/index.js
@@ -1,7 +1,9 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
+import { Menu, Button, Divider } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-import CourseContent from './CourseContent'
-import SeachBar from '../../SearchBar'
+import CourseContent from "./CourseContent";
+// import SeachBar from "../../SearchBar";
 
 const result = [1, 2, 3, 4];
 const results = result.map(() => <CourseContent />);
@@ -9,10 +11,18 @@ const results = result.map(() => <CourseContent />);
 const CourseUser = () => {
   return (
     <Fragment>
-      <SeachBar />
+      <Menu.Item />
+      <Button
+        name="search"
+        as={Link}
+        to="/search"
+        positive
+        content="Search New Course"
+      />
+      <Divider />
       {results}
     </Fragment>
-  )
-}
+  );
+};
 
-export default CourseUser
+export default CourseUser;
